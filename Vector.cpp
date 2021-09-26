f09790ada1529c2ebcf2975386f52ea5dd2a0ebb
kiranchindam <kiranchindam432@gmail.com>
2021-09-26 18:37:37 +0530

    Create Vector.cpp

---
+++ Vector.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  vector<int> v1;
+  cout<<"vector v1 capacity:"<<v1.capacity<<endl;
+  cout<<"vector v1 size:"<<v1.size<<endl;
+  v1.push_back(10);
+  v1.push_back(20);
+  v1.push_back(30);
+  cout<<"vector v1 capacity:"<<v1.capacity<<endl;
+  cout<<"vector v1 size:"<<v1.size<<endl;
+  for(unsigned int i=0;i<v1.size();i++)
+    cout<<v1[i]<<" ";
+  cout<<endl;
+  v1.push_back(40);
+  v1.push_back(50);
+  cout<<"vector v1 capacity:"<<v1.capacity<<endl;
+  cout<<"vector v1 size:"<<v1.size<<endl;
+  for(unsigned int i=0;i<v1.size();i++)
+    cout<<v1[i]<<" ";
+  cout<<endl;
+  return 0;
+}
+
+  
